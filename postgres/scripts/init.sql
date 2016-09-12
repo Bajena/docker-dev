108dcf6b18d7658d11b497ae84db8fed5c7fbfac
Orest Kulik <okulik@gmail.com>
2016-09-12 13:14:27 +0200

    Add postgresql service

---
+++ postgres/scripts/init.sql
@@ -0,0 +1,5 @@
+CREATE USER datolite;
+CREATE DATABASE datolite_development;
+CREATE DATABASE datolite_test;
+GRANT ALL PRIVILEGES ON DATABASE datolite_development TO datolite;
+GRANT ALL PRIVILEGES ON DATABASE datolite_test TO datolite;
